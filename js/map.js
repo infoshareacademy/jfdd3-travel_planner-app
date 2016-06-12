9fb6801f802830f38258465eab7808d7750ef550
Christopher Jagielski <k.jagielski@gmail.com>
2016-06-12 14:26:09 +0200

    Marker loading done.

--- js/map.js
+++ js/map.js
@@ -1,14 +1,26 @@
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 54.348545, lng: 18.6510408},
+        center: {lat: 54.348536, lng: 18.653224},
         zoom: 17
     });
 
+    var infowindow = new google.maps.InfoWindow;
 
-    var marker = new google.maps.Marker({
-        position: {lat: 54.348545, lng: 18.6510408},
-        map: map,
-        title: 'Hello World!'
-    });
+
+    var marker, i;
+
+    for (i = 0; i < objects.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(objects[i].position[0], objects[i].position[1]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(objects[i].name);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
 }
