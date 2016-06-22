3eadd1c84203ee3ebf6cd690c17f75d335747c82
rpoplawski <radoslaw.poplawski@gazeta.pl>
2016-06-22 16:26:15 +0200

    commit angular

--- angular/app.js
+++ angular/app.js
@@ -1,21 +1,37 @@
 (function(){
     var app = angular.module('travelApp', []);
     app.controller('buttonsCtrl',funcButtonCtrl);
-    app.controller('mainCtrl', funcTravelCtrl);
+    app.controller('mainCtrl', funcMainCtrl);
     app.directive('navbar',showNavbar);
     app.directive('map',showMap);
     app.directive('tiles',showTiles);
 
 
-    function funcButtonCtrl($scope) {
-        $scope.monuments = objects;
-        $scope.expandButtonStart = function() {
-            console.log('aaa');
-        }
+    /* app controller for navigation buttons */
+    function funcButtonCtrl() {
+        var bc = this;
+        bc.showMap = true;
+        bc.monuments = objects;
+        bc.views = ['Mapa','Kafelki'];
+        bc.startPoint = 'Punkt startowy';
+        bc.endPoint = 'Punkt końcowy';
+        bc.currView = 'Zmień widok';
+        bc.setStart = function(place){
+            bc.startPoint = place;
+        };
+        bc.setEnd = function(place){
+            bc.endPoint = place;
+        };
+        bc.setView = function(view) {
+            bc.currView = view;
+            view === 'Mapa' ? bc.showMap = true : bc.showMap=false;
+        };
+
     }
 
-    function funcTravelCtrl($scope){
-
+    /* app controller for main content */
+    function funcMainCtrl(){
+        var mc = this;
     }
 
     function showNavbar() {
